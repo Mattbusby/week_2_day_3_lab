ab6ddd7a51a51c9d45c2e6d3d80cadad1b493c5a
Matt <mattbusby86@gmail.com>
2022-01-12 14:01:32 +0000

    pub has name, till, drinks

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -3,10 +3,16 @@ from src.pub import Pub
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.pub = Pub("The Prancing Pony", 100.00, ["Pint", "shot", "can"])
+        self.pub = Pub("Basic Bar", 1000.00, ["Pint", "shot", "can"])
 
-    # def test_pub_has_name(self):
-    #     self.assertEqual("The Prancing Pony", self.pub.name)
+    def test_pub_has_name(self):
+        self.assertEqual("Basic Bar", self.pub.name)
+
+    def test_pub_has_till(self):
+        self.assertEqual(1000, self.pub.till)
+
+    def test_pub_has_drinks(self):
+        self.assertEqual(["Pint", "shot", "can"], self.pub.drinks)
 
     # def test_increase_till(self):
     #     self.pub.increase_till(2.50)
